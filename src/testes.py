71eec6270b74d27e5d87a85c00b68124c078e166
viinnyt <vini200982craft@gmail.com>
2017-09-06 21:47:55 -0300

    versão 1

--- src/testes.py
+++ src/testes.py
@@ -1,19 +1,22 @@
 import jogovelha
 import sys
+
 erroInicializar = False
+
 jogo = jogovelha.inicializar()
+
 if len(jogo) != 3:
-	erroInicializar = True
+    erroInicializar = True
 else:
-	for linha in jogo:
-		if len(linha) != 3:
-			erroInicializar = True
-		else:
-			for elemento in linha:
-				if elemento != '.':
-					erroInicializar = True
+    for linha in jogo:
+        if len(linha) != 3:
+            erroInicializar = True
+        else:
+            for elemento in linha:
+                if elemento != '.':
+                    erroInicializar = True
 
 if erroInicializar:
-	sys.exit(1)
+ sys.exit(1)
 else:
-	sys.exit(0)
+ sys.exit(0)
